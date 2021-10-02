7e88f9b72f29b067a2a1e57d61f339b77c4d87dd
carlosamuel8 <carlos.samuel.988711@gmail.com>
2021-10-02 11:49:48 -0300

    atividade1

---
+++ ATIVIDADE 1/teste.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+auto soma(auto a, auto b){
+    return a+b;
+}
+int main(){
+    cout << soma(5, 6) << endl;
+    cout << "testANDO" << endl;
+    return 0;
+}
