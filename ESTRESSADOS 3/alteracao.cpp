94fd981e08cb516b6557edbbd9394f2eb96ab171
carlosamuel8 <carlos.samuel.988711@gmail.com>
2021-10-24 20:14:36 -0300

    digitacao

--- ESTRESSADOS 3/alteracao.cpp
+++ ESTRESSADOS 3/alteracao.cpp
@@ -3,25 +3,15 @@
 #include<vector>
 #include <algorithm>
 
-int quantos_casais(std::vector<int> v){
-  int cont {0};
-
-  for(int i = 0; i < (int) v.size() ; i++){
-    for(int j = i + 1; j < (int) v.size(); j++){
-        if( abs(v[i]) == abs(v[j]) && v[i] != v[j])
-          cont++;
-    }
-  }
-  return cont;
+void remove(std::vector<int>& v, int x) {
+  v.erase(std::find(v.begin(), v.end(), x));
 }
 
 void insert(std::vector<int>& v, int x, int pos) {
   v.insert(v.begin() + pos, x);
 }
 
-void remove(std::vector<int>& v, int x) {
-  v.erase(std::find(v.begin(), v.end(), x));
-}
+
 
 int main(){
 
@@ -30,8 +20,14 @@ int main(){
 
    // std::cout << "casais: " << quantos_casais(vetor)<<"\n";
 
+    remove(vetor, 5);
+    std::cout << "\nremovendo: ";
+    for (size_t i = 0; i < vetor.size(); ++i) {
+        std::cout << vetor.at(i) << " ";
+    }
+
     insert(vetor, 5, 0);
-    std::cout << "\nrecorrentes: ";
+    std::cout << "\ninserindo: ";
     for (size_t i = 0; i < vetor.size(); ++i) {
         std::cout << vetor.at(i) << " ";
     }
---
+++ LAPISEIRA/lapiseira.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+
+struct 
+
+int main(){
+
+}
