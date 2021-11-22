d5b402430d598fe3cf4fa34fa69ccc7cd050f0b0
carlosamuel8 <carlos.samuel.988711@gmail.com>
2021-11-22 10:19:37 -0300

    contato

--- CINEMA/cinema.cpp
+++ CINEMA/cinema.cpp
@@ -1,115 +1,105 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
+#include <string>
+#include <sstream>
+
+class Fone{
+    std::string id;
+    std::string number;
 
 
-class Cliente{
-    std::string fone;
-    std::string id_nome;
-    
 public:
-    Cliente (std::string id_nome = "", std::string fone = "") : id_nome {id_nome}, fone {fone} {
+    Fone(std::string id="-", std::string number="-"){
+        this->id = id;
+        this->number = number;
     }
 
-    std::string getFone(){
-        return fone;
-    }
+    std::string getId(){
+        return this->id;
+    };
 
-    std::string getId_Nome(){
-        return id_nome;
-    }
+    std::string getNumber(){
+        return this->number;
+    };
 
-    std::string setFone(){
-        this->fone=fone;
-    } 
+    void setId(std::string id){
+        this->id = id;
+    };
 
-    std::string setId_nome(){
-        this->id_nome=id_nome;
-    } 
-
-
+    void setNumber(std::string number){
+        this->number = number;
+    };
 };
 
-
-class Sala {
-    std::vector<Cliente> cadeiras;
+class Contact{
+    std::string prefix = "-";
+    std::vector<Fone> fones;
+    std::string name;
 
 public:
-    Sala (int capacidade = 0) {
-        this->iniciar(capacidade);
+    Contact(std::string name = ""){
+        this->name = name;
     }
 
-    void iniciar (int capacidade) {
-        std::vector<Cliente> auxiliar;
-        for (int i{0}; i < capacidade; i++) {
-            Cliente cliente;
-            auxiliar.push_back(cliente);
-        }
-        this->cadeiras = auxiliar;
+    void addFone(Fone fone){
+        fones.push_back(fone);
+    }
+   
+    void rmFone(int index){
+        fones.erase(fones.begin() + index);
     }
 
-    void cancelar (std::string id) {
-        bool teste = true;
-        for (auto& i : this->cadeiras) {
-            if (i.getId_Nome() == id) {
-                i.getId_Nome();
-                i.setFone();
-                teste = false;
-                break;
-            }
+    std::string toString(){
+        std::stringstream str;
+        str << prefix << " " << this->name << "\n";
+        for(int i = 0; i < fones.size(); i++){
+            str << "[" << i << " : " << fones[i].getId() << " : " << fones[i].getNumber() << "]\n";
         }
-        if (teste == true) {
-            std::cout << "Operacao invalida, o cliente nao esta no cinema\n";
-        }
-
+        return str.str();
     }
 
-    bool reservar(std::string id, std::string fone, int ind) {
-        if (ind >= this->cadeiras.size()) {
-            std::cout << "Cadeira invalida \n";
-            return false;
+    std::string getName(){
+        return this->name;
+    }
+
+    void setName(std::string name){
+        this->name = name;
+    }
+};
+
+int main(){
+    Contact contato;
+    Fone fone;
+
+    while(true){
+        std::string str;
+        std::cin >> str;
+        if(str == "init"){
+            std::string name;
+            std::cin >> name;
+            contato = Contact(name);
+
+        }else if(str == "add"){
+            std::string id;
+            std::string number;
+            std::cin >> id >> number;
+            contato.addFone(fone);
         }
 
-        for (auto& i : this->cadeiras) {
-            if (i.getId_Nome() == id) {
-                std::cout << "Cliente ja esta no cinema \n";
-                return false;
-            }
-        }
-
-        if (this->cadeiras[ind].setId_nome() == "-") {
-            this->cadeiras[ind].setFone();
-            this->cadeiras[ind].setId_nome();
-            return true;
+        else if(str == "rm"){
+            int indice;
+            std::cin >> indice;
+            contato.rmFone(indice);
         }
         
-        std::cout << "fail: cadeira ja esta ocupada\n";
-        return false;
+        else if(str == "show"){
+            std::cout << contato.toString() << std::endl;
+        }
+        else if(str == "end"){
+            break;
+        }
+        else{
+            std::cout << "-Comando invalido-" << std::endl;
+        }
     }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
